3aa2c293dae900a5fa469332a8027cabfac75a99
John Mack <liljmack05@gmail.com>
2021-01-26 22:16:48 -0600

    Added Groups and Catalog Endpoints to API Wrapper

--- venv/RobloxAPI.py
+++ venv/RobloxAPI.py
@@ -100,8 +100,117 @@ class Games:
         cookies = readCookie()
         r = requests.get(url, cookies=cookies)
         j = json.loads(r.text)
-        return jc
-        
+        return j
+class Groups:
+    def getGroupInfo(groupId):
+        """"""
+        url = f"https://groups.roblox.com/v1/groups/{groupId}"
+        r = requests.get(url)
+        j = json.loads(r.text)
+        return j
+class Catalog:
+    def Categories():
+        """Returns all possible categories."""
+        cat = {
+    	    "Featured": 0,
+    	    "All": 1,
+    	    "Collectibles": 2,
+    	    "Clothing": 3,
+    	    "BodyParts": 4,
+    	    "Gear": 5,
+    	    "Models": 6,
+    	    "Plugins": 7,
+	        "Decals": 8,
+    	    "Audio": 9,
+    	    "Meshes": 10,
+	        "Accessories": 11,
+	        "AvatarAnimations": 12,
+	        "CommunityCreations": 13,
+	        "Video": 14,
+	        "Recommended": 15
+        }
+        return cat
+    def Subcategories():
+        """Returns all possible subcategories."""
+        subcat = {
+    	"Featured": 0,
+    	"All": 1,
+    	"Collectibles": 2,
+    	"Clothing": 3,
+    	"BodyParts": 4,
+    	"Gear": 5,
+    	"Models": 6,
+    	"Plugins": 7,
+    	"Decals": 8,
+    	"Hats": 9,
+    	"Faces": 10,
+    	"Packages": 11,
+    	"Shirts": 12,
+    	"Tshirts": 13,
+    	"Pants": 14,
+    	"Heads": 15,
+    	"Audio": 16,
+    	"RobloxCreated": 17,
+    	"Meshes": 18,
+    	"Accessories": 19,
+    	"HairAccessories": 20,
+    	"FaceAccessories": 21,
+    	"NeckAccessories": 22,
+    	"ShoulderAccessories": 23,
+    	"FrontAccessories": 24,
+    	"BackAccessories": 25,
+    	"WaistAccessories": 26,
+    	"AvatarAnimations": 27,
+    	"ClimbAnimations": 28,
+    	"FallAnimations": 30,
+    	"IdleAnimations": 31,
+	    "JumpAnimations": 32,
+	    "RunAnimations": 33,
+    	"SwimAnimations": 34,
+    	"WalkAnimations": 35,
+    	"AnimationPackage": 36,
+    	"Bundles": 37,
+    	"AnimationBundles": 38,
+	    "EmoteAnimations": 39,
+	    "CommunityCreations": 40,
+	    "Video": 41,
+	    "Recommended": 51
+    }
+        return subcat
+
+    def listExclusiveItems(appStore):
+        """Returns all exclusive items for 1 of 4 app stores; Xbox, Amazon, Google Play or iOS."""
+        appStores = ('Xbox', 'Amazon', 'iOS', 'Google Play')
+        if appStore not in appStores:
+            Exception(f"No valid app store was provided. Valid choices are {appStores}.")
+        else:
+            url = "https://catalog.roblox.com/v1/exclusive-items/{appStore}/bundles"
+            r = requests.get(url)
+            j = json.loads(r.text)
+            return j['data']
+    def showOwnedBundles(userId):
+        """Returns a list of all bundles owned by a specified ID. The user MUST have a public inventory or this function won't work."""
+        url = f"https://catalog.roblox.com/v1/users/{userId}/bundles?limit=100&sortOrder=Asc"
+        r = requests.get(url)
+        j = json.loads(r.text)
+        return j['data']
+    def showUserInventory(userId, assetType):
+        """Returns the user inventory from the specified user ID. Player MUST have a public inventory for this to work."""
+        validTypes = {'Image', 'TShirt', 'Audio', 'Mesh', 'Lua', 'Hat', 'Place','Model','Shirt','Pants','Decal','Head','Face', 'Gear', 'Badge', 'Animation', 'Torso', 'RightArm', 'LeftArm', 'LeftLeg', 'RightLeg', 'Package', 'GamePass', 'Plugin','MeshPart','HairAccessory', 'FaceAccesory', 'NeckAccessory', 'ShoulderAccessory', 'FrontAccessory', 'BackAccessory', 'WaistAccessory', 'ClimbAnimation', 'DeathAnimation', 'FallAnimation', 'IdleAnimation', 'JumpAnimation', 'RunAnimation', 'SwimAnimation', 'WalkAnimation', 'PoseAnimation', 'EarAccessory', 'EyeAccessory', 'EmoteAnimation', 'Video'}
+        if assetType not in validTypes:
+            Exception(f"You have entered an invalid asset type to show inventory for. Currently, the valid asset types are: {validTypes}")
+            return
+        else:
+            url = f"https://inventory.roblox.com/v2/users/{userId}/inventory?assetTypes={assetType}&limit=100&sortOrder=Desc"
+            r = requests.get(url)
+            j = json.loads(r.text)
+            return j['data']
+
+
+
+
+
+
         
         
         
